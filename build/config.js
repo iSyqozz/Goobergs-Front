3b1f6bbefe50a36706b8d402661e8497a44af9e2
Fares Hassen <fares.hassen512@gmail.com>
2023-07-08 16:30:54 +0100

    prod

--- build/config.js
+++ build/config.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-exports.config = 'http://192.168.1.43:3000';
+exports.config = 'https://api.wabalabaland.com';
